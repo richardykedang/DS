6df59161a483576b5942abae438f8bea75058f3c
Alvinuardi Hutomo <44694587+Alvin201@users.noreply.github.com>
2023-05-23 22:51:38 +0700

    integrasi api user

--- DigiProj.Shared/Dtos/Responses/MsUser/UserResponse.cs
+++ DigiProj.Shared/Dtos/Responses/MsUser/UserResponse.cs
@@ -1,4 +1,5 @@
-﻿using Newtonsoft.Json;
+﻿using Digiproj.Shared.Dtos.Responses;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -54,6 +55,11 @@ namespace DigiProj.Shared.Dtos.Responses.MsUser
 	{
 		public Guid Id { get; set; }
 		public string EmployeeId { get; set; }
+		public string Name { get; set; }
+		public string UserName { get; set; }
+		public string Email { get; set; }
+		public bool IsActive { get; set; }
+		public RoleResponse[] Roles { get; set; }
 
 	}
 }
